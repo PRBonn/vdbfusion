a9d139288f2ee1743daa88d05ec630943b0549c9
Sam Thomas <samthomasdigital@gmail.com>
2022-04-14 10:56:48 +1000

    Added google test

--- src/vdbfusion/vdbfusion/VDBVolume.h
+++ src/vdbfusion/vdbfusion/VDBVolume.h
@@ -8,52 +8,53 @@
 
 namespace vdbfusion {
 
-class VDBVolume {
-public:
-    VDBVolume(float voxel_size, float sdf_trunc, bool space_carving_ = false);
-    ~VDBVolume() = default;
+    class VDBVolume {
+    public:
+        VDBVolume(float voxel_size, float sdf_trunc, bool space_carving_ = false);
 
-public:
-    /// @brief Integrates a new (globally aligned) PointCloud into the current
-    /// tsdf_ volume.
-    void Integrate(const std::vector<Eigen::Vector3d>& points,
-                   const Eigen::Vector3d& origin,
-                   const std::function<float(float)>& weighting_function);
+        ~VDBVolume() = default;
 
-    /// @brief Integrates a new (globally aligned) PointCloud into the current
-    /// tsdf_ volume.
-    void inline Integrate(const std::vector<Eigen::Vector3d>& points,
-                          const Eigen::Matrix4d& extrinsics,
-                          const std::function<float(float)>& weighting_function) {
-        const Eigen::Vector3d& origin = extrinsics.block<3, 1>(0, 3);
-        Integrate(points, origin, weighting_function);
-    }
+    public:
+        /// @brief Integrates a new (globally aligned) PointCloud into the current
+        /// tsdf_ volume.
+        void Integrate(const std::vector<Eigen::Vector3d> &points,
+                       const Eigen::Vector3d &origin,
+                       const std::function<float(float)> &weighting_function);
 
-    /// @brief Integrate incoming TSDF grid inside the current volume using the TSDF equations
-    void Integrate(openvdb::FloatGrid::Ptr grid,
-                   const std::function<float(float)>& weighting_function);
+        /// @brief Integrates a new (globally aligned) PointCloud into the current
+        /// tsdf_ volume.
+        void inline Integrate(const std::vector<Eigen::Vector3d> &points,
+                              const Eigen::Matrix4d &extrinsics,
+                              const std::function<float(float)> &weighting_function) {
+            const Eigen::Vector3d &origin = extrinsics.block<3, 1>(0, 3);
+            Integrate(points, origin, weighting_function);
+        }
 
-    /// @brief Fuse a new given sdf value at the given voxel location, thread-safe
-    void UpdateTSDF(const float& sdf,
-                    const openvdb::Coord& voxel,
-                    const std::function<float(float)>& weighting_function);
+        /// @brief Integrate incoming TSDF grid inside the current volume using the TSDF equations
+        void Integrate(openvdb::FloatGrid::Ptr grid,
+                       const std::function<float(float)> &weighting_function);
 
-    /// @brief Prune TSDF grids, ideal utility to cleanup a D(x) volume before exporting it
-    openvdb::FloatGrid::Ptr Prune(float min_weight) const;
+        /// @brief Fuse a new given sdf value at the given voxel location, thread-safe
+        void UpdateTSDF(const float &sdf,
+                        const openvdb::Coord &voxel,
+                        const std::function<float(float)> &weighting_function);
 
-    /// @brief Extracts a TriangleMesh as the iso-surface in the actual volume
-    [[nodiscard]] std::tuple<std::vector<Eigen::Vector3d>, std::vector<Eigen::Vector3i>>
-    ExtractTriangleMesh(bool fill_holes = true, float min_weight = 0.5) const;
+        /// @brief Prune TSDF grids, ideal utility to cleanup a D(x) volume before exporting it
+        openvdb::FloatGrid::Ptr Prune(float min_weight) const;
 
-public:
-    /// OpenVDB Grids modeling the signed distance field and the weight grid
-    openvdb::FloatGrid::Ptr tsdf_;
-    openvdb::FloatGrid::Ptr weights_;
+        /// @brief Extracts a TriangleMesh as the iso-surface in the actual volume
+        [[nodiscard]] std::tuple<std::vector<Eigen::Vector3d>, std::vector<Eigen::Vector3i>>
+        ExtractTriangleMesh(bool fill_holes = true, float min_weight = 0.5) const;
 
-    /// VDBVolume public properties
-    float voxel_size_;
-    float sdf_trunc_;
-    bool space_carving_;
-};
+    public:
+        /// OpenVDB Grids modeling the signed distance field and the weight grid
+        openvdb::FloatGrid::Ptr tsdf_;
+        openvdb::FloatGrid::Ptr weights_;
+
+        /// VDBVolume public properties
+        float voxel_size_;
+        float sdf_trunc_;
+        bool space_carving_;
+    };
 
 }  // namespace vdbfusion
---
+++ src/vdbfusion/vdbfusion/tests/VDBVolumeTest.cpp
@@ -0,0 +1,34 @@
+#include "../VDBVolume.h"
+#include "gtest/gtest.h"
+
+// OpenVDB
+#include <openvdb/openvdb.h>
+
+class VDBVolumeFixture : public ::testing::Test {
+protected:
+    void SetUp() override { openvdb::initialize(); }
+};
+
+TEST_F(VDBVolumeFixture, IntegrateFast) {
+    float voxel_size = 0.1;
+    float sdf_trunc = 0.3;
+    bool space_carving = false;
+
+    vdbfusion::VDBVolume tsdf_volume(voxel_size, sdf_trunc, space_carving);
+
+    const Eigen::Vector3d origin(1.0, 3.0, 2.0);
+
+    // Generate points on a sphere
+    std::vector<Eigen::Vector3d> points;
+    for (int i = 0; i < 1000000; i++) {
+        Eigen::Vector3d p;
+        p[0] = (float) rand() / RAND_MAX;
+        p[1] = (float) rand() / RAND_MAX;
+        p[2] = (float) rand() / RAND_MAX;
+        p = p.normalized() * 0.5;
+        points.emplace_back(p);
+    }
+
+    // Integrate the points
+    tsdf_volume.Integrate(points, origin, [](float /*unused*/) { return 1.0; });
+}
