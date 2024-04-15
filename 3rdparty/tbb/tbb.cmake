24440a435a050dbeb2f4f751dbd94b2565ff3b64
Luca Lobefaro <luca.lobefaro95@gmail.com>
2024-04-15 15:20:30 +0200

    TBB version updated because does not compile on my.

--- 3rdparty/tbb/tbb.cmake
+++ 3rdparty/tbb/tbb.cmake
@@ -24,17 +24,14 @@ include(ExternalProject)
 ExternalProject_Add(
   external_tbb
   PREFIX tbb
-  URL https://github.com/nachovizzo/tbb/archive/refs/tags/tbbstatic.tar.gz
-  URL_HASH SHA256=db5ede77c4bd10ad12fab11ed38b7e8cf80aba85db16a57514073c383e6c8630
+  URL https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.4.1.tar.gz
   UPDATE_COMMAND ""
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
              ${ExternalProject_CMAKE_ARGS}
              ${ExternalProject_CMAKE_CXX_FLAGS}
              # custom flags
-             -DTBB_BUILD_TBBMALLOC=ON
-             -DTBB_BUILD_SHARED=OFF
-             -DTBB_BUILD_STATIC=ON
-             -DTBB_BUILD_TESTS=OFF)
+             -DBUILD_SHARED_LIBS=OFF
+             -DTBB_TEST=OFF)
 
 # Simulate importing TBB::tbb for OpenVDBHelper target
 ExternalProject_Get_Property(external_tbb INSTALL_DIR)
