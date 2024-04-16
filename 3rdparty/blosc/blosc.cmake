17a442c4bf34c59472a93bef912378548ae1e466
Meher Malladi <rm.meher97@gmail.com>
2024-04-16 13:24:27 +0200

    blosc is fetch content-ed

--- 3rdparty/blosc/blosc.cmake
+++ 3rdparty/blosc/blosc.cmake
@@ -19,22 +19,40 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-set(BUILD_STATIC ON)
-set(BUILD_TESTS OFF)
-set(BUILD_BENCHMARKS OFF)
-set(PREFER_EXTERNAL_COMPLIBS OFF)
+set(BUILD_STATIC
+    ON
+    CACHE BOOL "static build")
+set(BUILD_SHARED
+    OFF
+    CACHE BOOL "static build")
+set(BUILD_TESTS
+    OFF
+    CACHE BOOL "tests")
+set(BUILD_BENCHMARKS
+    OFF
+    CACHE BOOL "benchmarks")
+set(PREFER_EXTERNAL_COMPLIBS
+    OFF
+    CACHE BOOL "external_complibs")
 
 include(FetchContent)
-FetchContent_Declare(blosc URL https://github.com/Blosc/c-blosc/archive/refs/tags/v1.5.0.tar.gz)
+FetchContent_Declare(
+  blosc URL https://github.com/Blosc/c-blosc/archive/refs/tags/v1.21.5.tar.gz)
 if(NOT blosc_POPULATED)
   FetchContent_Populate(blosc)
   if(${CMAKE_VERSION} GREATER_EQUAL 3.25)
-    add_subdirectory(${blosc_SOURCE_DIR} ${blosc_BINARY_DIR} SYSTEM EXCLUDE_FROM_ALL)
+    add_subdirectory(${blosc_SOURCE_DIR} ${blosc_BINARY_DIR} SYSTEM
+                     EXCLUDE_FROM_ALL)
   else()
-    # Emulate the SYSTEM flag introduced in CMake 3.25. Withouth this flag the compiler will
-    # consider this 3rdparty headers as source code and fail due the -Werror flag.
+    # Emulate the SYSTEM flag introduced in CMake 3.25. Withouth this flag the
+    # compiler will consider this 3rdparty headers as source code and fail due
+    # the -Werror flag.
     add_subdirectory(${blosc_SOURCE_DIR} ${blosc_BINARY_DIR} EXCLUDE_FROM_ALL)
-    get_target_property(blosc_include_dirs blosc INTERFACE_INCLUDE_DIRECTORIES)
-    set_target_properties(blosc PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${blosc_include_dirs}")
+    get_target_property(blosc_include_dirs blosc_static
+                        INTERFACE_INCLUDE_DIRECTORIES)
+    set_target_properties(
+      blosc_static PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES
+                              "${blosc_include_dirs}")
   endif()
+  add_library(Blosc::blosc ALIAS blosc_static)
 endif()
